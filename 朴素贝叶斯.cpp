0d769008335671ce00fc0e7106367cb86207d35f
ZhiYu <zhiyu-kld@qq.com>
2017-08-22 19:10:18 +0800

    读取dat文件

---
+++ 朴素贝叶斯.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>  
+#include <set>  
+#include <vector>  
+#include<fstream>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+ifstream datafile("G:\\im\\titanic.dat");		
+
+ struct sdata
+{
+	double Class;
+	double Age;
+	double Sex;
+	int survive;
+}d;
+
+vector<sdata> V_sdata;
+
+int main()
+{
+	string line;
+	double Class, Age, Sex;
+	int survive;
+	while (getline(datafile,line))
+	{
+		cout << line << endl;
+		sscanf(const_cast<char*>(line.c_str()), "%lf,%lf,%lf,%d", &Class, &Age, &Sex, &survive);
+		d = { Class,Age,Sex,survive };
+		V_sdata.push_back(d);
+	}
+	system("pause");
+	return 0;
+}
