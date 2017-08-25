5a215113cfb3aadb7ee59d129d2b86d9ab7ba4a4
ZhiYu <zhiyu-kld@qq.com>
2017-08-25 22:43:23 +0800

    大幅修改，主代码测试成功

--- 朴素贝叶斯.cpp
+++ 朴素贝叶斯.cpp
@@ -10,36 +10,30 @@ using namespace std;
 ifstream datafile("G:\\im\\titanic.dat");		
 
 
-multiset<double> Val_Survive;
 multiset<double> Val_Class, Val_Age, Val_Sex;			//各属性所有值
+multiset<double> Val_Class_, Val_Age_, Val_Sex_;
 
-vector<double> Val_class, Val_age, Val_sex, Val_survive;
-
-set<double> surviveset;
 set<double> Classset, Ageset, Sexset;			//各属性所有可能的值
+set<double> Classset_, Ageset_, Sexset_;
+
+vector<double> Classvector,Agevector, Sexvector;
+vector<double> Classvector_, Agevector_, Sexvector_;
+
+vector<double>::iterator it2,it1;				//迭代器
+set<double>::iterator it0;
 
 
-vector<double>::iterator it0,it1;
-set<double>::iterator it2,it3,it4;
-
-set<double>::iterator it;
 
 double Py_1, Py1;
-
 
 int main()
 {
 	int all_num = 0;			//训练数据总数
+	int num_1 = 0;
 	string line;				//每行的字符
 	double Class, Age, Sex;		//属性值
 	double survive;				//待分类属性
 
-
-
-	Val_Age.clear();			//multiset初始化
-	Val_Class.clear();
-	Val_Sex.clear();
-	Val_Survive.clear();
 
 	cout << "正在分析概率..." << endl;
 
@@ -48,109 +42,105 @@ int main()
 		cout << line << endl;
 		if (line[0] == '@') continue;
 		sscanf(const_cast<char*>(line.c_str()), "%lf,%lf,%lf,%lf", &Class, &Age, &Sex, &survive);
+		
+		if(survive==1.0)
+		{ 
+			Classset.insert(Class);				//set
+			Ageset.insert(Age);
+			Sexset.insert(Sex);
 
-		Classset.insert(Class);				//set
-		Ageset.insert(Age);
-		Sexset.insert(Sex);
-		surviveset.insert(survive);
+			Val_Class.insert(Class);			//multiset
+			Val_Age.insert(Age);
+			Val_Sex.insert(Sex);
 
-		Val_Class.insert(Class);			//multiset
-		Val_Age.insert(Age);
-		Val_Sex.insert(Sex);
-		Val_Survive.insert(survive);
+			Classvector.push_back(Class);			//vector
+			Agevector.push_back(Age);
+			Sexvector.push_back(Sex);
+			
+			num_1++;
+		}
+		else
+		{
+			Classset_.insert(Class);				//set
+			Ageset_.insert(Age);
+			Sexset_.insert(Sex);
 
-		Val_class.push_back(Class);			//multiset
-		Val_age.push_back(Age);
-		Val_sex.push_back(Sex);
-		Val_survive.push_back(survive);
+			Val_Class_.insert(Class);			//multiset
+			Val_Age_.insert(Age);
+			Val_Sex_.insert(Sex);
+
+			Classvector_.push_back(Class);			//vector
+			Agevector_.push_back(Age);
+			Sexvector_.push_back(Sex);
+		}
 
 		all_num++;
 	}
 
-	vector<double> Classvector;
-	vector<double> Agevector;
-	vector<double> Sexvector;
-
-	copy(Classset.begin(), Classset.end(), back_inserter( Classvector));
-	copy(Ageset.begin(), Ageset.end(), back_inserter(Agevector));
-	copy(Sexset.begin(), Sexset.end(), back_inserter(Sexvector));
 
 
-	Py1 = Val_Survive.count(1.0) / (double)Val_Survive.size();		//P(y=1)
-	Py_1 = Val_Survive.count(-1.0) / (double)Val_Survive.size();										//P(y=-1)
+	Py1 =num_1 / (double)all_num;		//P(y=1)
+	Py_1 = 1 - Py1;						//P(y=-1)
 
 	cout << "P(y=1)=" << Py1 << endl;
 	cout << "P(y=-1)=" << Py_1 << endl;
 
 	//条件概率
 
+	vector<double> P_Class, P_Age, P_Sex;
+	vector<double> P_Class_, P_Age_, P_Sex_;
+	vector<double> Class_set_vector, Age_set_vector, Sex_set_vector;
+	vector<double> Class_set_vector_, Age_set_vector_, Sex_set_vector_;
+
+	copy(Classset.begin(), Classset.end(), back_inserter(Class_set_vector));
+	copy(Ageset.begin(), Ageset.end(), back_inserter(Age_set_vector));
+	copy(Sexset.begin(), Sexset.end(), back_inserter(Sex_set_vector));
+	copy(Classset_.begin(), Classset_.end(), back_inserter(Class_set_vector_));
+	copy(Ageset_.begin(), Ageset_.end(), back_inserter(Age_set_vector_));
+	copy(Sexset_.begin(), Sexset_.end(), back_inserter(Sex_set_vector_));
+
 	//y=1时Class的条件概率
 	double PC0=1.0;
-	vector<double> P_Class;
-	it1 = Val_survive.begin();
-	for (it0 = Classvector.begin(); it0 != Classvector.end(); it0++)
+	for (it2 = Class_set_vector.begin(); it2 != Class_set_vector.end(); it2++)
 	{
-		if (*it1 == -1.0) { it1++; continue; }
-		PC0 = Val_Class.count(*it0) / (double)Val_Class.size();
+		PC0 = Val_Class.count(*it2) / (double)num_1;
 		P_Class.push_back(PC0);
-		it1++;
 	}
 
 	//y=1时Age的条件概率
-	vector<double> P_Age;
-	it1 = Val_survive.begin();
-	for (it0 = Agevector.begin(); it0 != Agevector.end(); it0++)
+	for (it2 = Age_set_vector.begin(); it2 != Age_set_vector.end(); it2++)
 	{
-		if (*it1 == -1.0) { it1++; continue; }
-		PC0 = Val_Age.count(*it0) / (double)Val_Age.size();
+		PC0 = Val_Age.count(*it2) / (double)num_1;
 		P_Age.push_back(PC0);
-		it1++;
 	}
 
 	//y=1时Sex的条件概率
-	vector<double> P_Sex;
-	it1 = Val_survive.begin();
-	for (it0 = Sexvector.begin(); it0 != Sexvector.end(); it0++)
+	for (it2 = Sex_set_vector.begin(); it2 != Sex_set_vector.end(); it2++)
 	{
-		if (*it1 == -1.0) { it1++; continue; }
-		PC0 = Val_Sex.count(*it0) / (double)Val_Sex.size();
+		PC0 = Val_Sex.count(*it2) / (double)num_1;
 		P_Sex.push_back(PC0);
-		it1++;
 	}
 
 	//y=-1时Class的条件概率
-	vector<double> P_Class_;
-	it1 = Val_survive.begin();
-	for (it0 = Classvector.begin(); it0 != Classvector.end(); it0++)
+	for (it2 = Class_set_vector_.begin(); it2 != Class_set_vector_.end(); it2++)
 	{
-		if (*it1 == 1.0) { it1++; continue; }
-		PC0 = Val_Class.count(*it0) / (double)Val_Class.size();
+		PC0 = Val_Class_.count(*it2) / (double)(all_num-num_1);
 		P_Class_.push_back(PC0);
-		it1++;
 	}
 
 
 	//y=-1时Age的条件概率
-	vector<double> P_Age_;
-	it1 = Val_survive.begin();
-	for (it0 = Agevector.begin(); it0 != Agevector.end(); it0++)
+	for (it2 = Age_set_vector_.begin(); it2 != Age_set_vector_.end(); it2++)
 	{
-		if (*it1 == 1.0) { it1++; continue; }
-		else
-			PC0 = Val_Age.count(*it0) / (double)Val_Age.size();
+			PC0 = Val_Age_.count(*it2) / (double)(all_num - num_1);
 		P_Age_.push_back(PC0);
-		it1++;
 	}
 
 	//y=-1时Sex的条件概率
-	vector<double> P_Sex_;
-	it1 = Val_survive.begin();
-	for (it0 = Sexvector.begin(); it0 != Sexvector.end(); it0++)
+	for (it2 = Sex_set_vector_.begin(); it2 != Sex_set_vector_.end(); it2++)
 	{
-		if (*it1 == 1.0) { it1++; continue; }
-		PC0 = Val_Sex.count(*it0) / (double)Val_Sex.size();
+		PC0 = Val_Sex_.count(*it2) / (double)(all_num - num_1);
 		P_Sex_.push_back(PC0);
-		it1++;
 	}
 
 
@@ -164,39 +154,39 @@ int main()
 	
 	//survive=1
 
-	for (unsigned int i = 0; i < Classvector.size(); i++)
+	for (unsigned int i = 0; i < Class_set_vector.size(); i++)
 	{
-		if (Classvector[i] == Class)
+		if (Class_set_vector[i] == Class)
 			P11 = P_Class[i];
 	}
 
-	for (unsigned int i = 0; i < Agevector.size(); i++)
+	for (unsigned int i = 0; i < Age_set_vector.size(); i++)
 	{
-		if (Agevector[i] == Age)
+		if (Age_set_vector[i] == Age)
 			P21 = P_Age[i];
 	}
 
-	for (unsigned int i = 0; i < Sexvector.size(); i++)
+	for (unsigned int i = 0; i < Sex_set_vector.size(); i++)
 	{
-		if (Sexvector[i] == Sex)
+		if (Sex_set_vector[i] == Sex)
 			P31 = P_Sex[i];
 	}
 	//survive=-1
-	for (unsigned int i = 0; i < Classvector.size(); i++)
+	for (unsigned int i = 0; i < Class_set_vector_.size(); i++)
 	{
-		if (Classvector[i] == Class)
+		if (Class_set_vector_[i] == Class)
 			P12 = P_Class_[i];
 	}
 
-	for (unsigned int i = 0; i < Agevector.size(); i++)
+	for (unsigned int i = 0; i < Age_set_vector_.size(); i++)
 	{
-		if (Agevector[i] == Age)
+		if (Age_set_vector_[i] == Age)
 			P22 = P_Age_[i];
 	}
 
-	for (unsigned int i = 0; i < Sexvector.size(); i++)
+	for (unsigned int i = 0; i < Sex_set_vector_.size(); i++)
 	{
-		if (Sexvector[i] == Sex)
+		if (Sex_set_vector_[i] == Sex)
 			P32 = P_Sex_[i];
 	}
 
